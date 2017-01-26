57d4ce2edd2c043b81eaf873fffb4b30048ab889
chris524 <chrismaledon@chriss-MacBook.local>
2017-01-25 21:58:27 -0700

    city change fixed

--- city change/index.js
+++ city change/index.js
@@ -4,40 +4,40 @@
 // based on the input
 $(document).ready(function(){
 
-    $("#submit-btn").change(changeBackground);
+    $("#submit-btn").click(changeBackground);
     function changeBackground(){
+        event.preventDefault();
     //define function that runs whent the button is clicked
         //get city name input from input element
         var cityInput = $("#city-type").val();
-        
         //San Fran
         if(cityInput == "san francisco" || cityInput == "sf" || cityInput == "bay city"){
-            $('body').css("images.sf.jpg");
+            $('body').css("background-image",'url(images/sf.jpg)');
             //if city input is SF, make background sf.jpg
         }
         
         //Austin
         else if ( cityInput == "austin" || cityInput == "atx"){
-            $('body').css("images.austin.jpg");
+            $('body').css("background-image",'url(images/austin.jpg)');
         
             //if city input is Austin, make background austin.jpg
         }
         
         //NYC
         else if( cityInput == "new york" || cityInput == "new york city" || cityInput == "nyc"){
-            $('body').css("images.nyc.jpg");
+            $('body').css("background-image",'url(images/nyc.jpg)');
             //if city input is NYC, make background NYC.jpg
         
         }
         //LA
         else if( cityInput == "los angeles" || cityInput == "la" || cityInput == "lax"){
-            $('body').css("images.la.jpg");
+            $('body').css("background-image",'url(images/la.jpg)');
         //if city input is LA, make background LA.jpg
         }
     
         //Sydney
         else if( cityInput == "sydney" || cityInput == "syd" ){
-            $('body').css("images.sydney.jpg");
+            $('body').css("background-image",'url(images/sydney.jpg")');
         //if city input is Sydney, make background sydney.jpg
         }
     };
